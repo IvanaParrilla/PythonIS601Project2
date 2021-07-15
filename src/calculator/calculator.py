10a4838a29494914d0372150a8edc9e24e7bf6bf
Ivana Parrilla <ip22@njit.edu>
2021-07-15 17:00:00 -0400

    Feature: Added additional calculator functions

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -4,6 +4,9 @@ from calculator.multiplication import multiplication
 from calculator.division import division
 from calculator.nthPower import nth_power
 from calculator.nthRoot import nth_root
+from calculator.aggregate import aggregate
+from calculator.nSquared import n_squared
+from calculator.squareRoot import square_root
 
 
 class Calculator:
@@ -28,6 +31,14 @@ class Calculator:
         self.result = division(a, b)
         return self.result
 
+    def sqr(self, a):
+        self.result = n_squared(a)
+        return self.result
+
+    def sqroot(self, a):
+        self.result = square_root(a)
+        return self.result
+
     def npow(self, a, n):
         self.result = nth_power(a, n)
         return self.result
@@ -35,7 +46,6 @@ class Calculator:
     def nroot(self, a, n):
         self.result = nth_root(a, n)
         return self.result
-
 
     def aggr(self, a):
         self.result = aggregate(a)
---
+++ src/calculator/nSquared.py
@@ -0,0 +1,2 @@
+def n_squared(a):
+    return a ** 2
