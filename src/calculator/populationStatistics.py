9b4d5e7183197fde34aa59dcda6fb5ca9cdcf996
Ivana Parrilla <ip22@njit.edu>
2021-07-15 16:19:40 -0400

    Feature: Updated Calculator and Variance

--- src/calculator/populationStatistics.py
+++ src/calculator/populationStatistics.py
@@ -1,8 +1,17 @@
-from calculator import Calculator
+from generalStatistics.generalStatistics import GeneralStatistics
 
 
-class PopulationStatistics(Calculator):
+class PopulationStatistics(GeneralStatistics):
 
     def __init__(self):
         super().__init__()
+        pass
+
+    def p_mean(self):
+        pass
+
+    def p_var(self):
+        pass
+
+    def p_std_dev(self):
         pass
--- src/tests/calculatorTest.py
+++ src/tests/calculatorTest.py
@@ -1,34 +1,32 @@
 import unittest
-from calculator import Calculator
+from calculator.calculator import Calculator
 
 
 class CalculatorTest(unittest.TestCase):
+
     def setUp(self) -> None:
         self.calc = Calculator()
+
     def test_object_instance(self):
         self.assertIsInstance(self.calc, Calculator)
 
     def test_addition_method(self):
-        self.assertEqual(self.calc.addition(1, 1), 2)
+        self.assertEqual(self.calc.add(1, 1), 2)
 
     def test_subtraction_method(self):
-        self.assertEqual(self.calc.subtraction(1, 3), 2)
+        self.assertEqual(self.calc.sub(3, 1), 2)
 
     def test_multiplication_method(self):
-        self.assertEqual(self.calc.muliplication(1, 2), 2)
+        self.assertEqual(self.calc.mul(1, 2), 2)
 
     def test_division_method(self):
-        self.assertAlmostEqual(self.calc.division(2, 4), 2.00000000)
+        self.assertAlmostEqual(self.calc.div(4, 2), 2.00000000)
 
-    def test_power_method_root(self):
-        self.assertEqual(self.calc.npow(4, 0.5), 2)
-
-    def test_power_method_integer(self):
+    def test_power_method(self):
         self.assertEqual(self.calc.npow(2, 2), 4)
 
-    def test_power_method_mixed(self):
-        self.assertEqual(self.calc.npow(4, 1.5), 8)
+    def test_root_method(self):
+        self.assertEqual(self.calc.nroot(4, 2), 2)
 
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
