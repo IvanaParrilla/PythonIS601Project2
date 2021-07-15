8902dffe5e2b6e47001db81cd83119bff6b4ee60
Ivana Parrilla <ip22@njit.edu>
2021-07-14 23:28:51 -0400

    Added: __init__.py

--- src/calculator/__init__.py
+++ src/calculator/__init__.py
@@ -1,4 +1,5 @@
 from calculator import Calculator
+from generalStatistics import GeneralStatistics
 from populationStatistics import PopulationStatistics
 from sampleStatistics import SampleStatistics
 
